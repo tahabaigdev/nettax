53e2e517d4374ecbd9f3850a2f58821e72b06450
Taha Baig <connectmtb@gmail.com>
2025-08-07 17:21:28 +0500

    tax calculator changes

--- src/pages/TaxCalculator.tsx
+++ src/pages/TaxCalculator.tsx
@@ -15,6 +15,11 @@ import {
 } from "@/components/ui/select";
 import { Label } from "@/components/ui/label";
 import { calculateYearlyTax } from "@/lib/taxUtils";
+import rays03 from "/images/rays-03.svg";
+import blueHighlight01 from "/images/blue-highlight-01.svg";
+import SectionTitle from "@/components/ui/SectionTitle";
+import SectionDescription from "@/components/ui/SectionDescription";
+import { ChevronRight } from "lucide-react";
 
 const TaxCalculator = () => {
   const [year, setYear] = useState("2025");
@@ -66,8 +71,8 @@ const TaxCalculator = () => {
         subheading="Quickly calculate your tax liabilities and optimize savings with compliant, strategic insights."
       />
 
-      <div className="px-[2rem] pt-[10rem] pb-[4rem] lg:py-[10rem] xl:px-[0rem]">
-        <div className="container flex flex-col gap-[4rem] lg:flex-row">
+      <div className="pt-[10rem] pb-[4rem] lg:py-[10rem]">
+        <div className="container flex flex-col gap-[4rem] px-[2rem] pb-[8rem] lg:flex-row xl:px-[0rem]">
           <div className="lg:w-[35%]">
             <h3 className="text-[3rem] leading-[3.5rem] font-light text-(--primary-color) md:text-[4rem] md:leading-[4.8rem]">
               Salary Tax{" "}
@@ -219,6 +224,46 @@ const TaxCalculator = () => {
                 />
               </div>
             </fieldset>
+          </div>
+        </div>
+
+        <div className="bg-[#EEF8FF] px-[2rem] py-[4rem] lg:py-[10rem] xl:px-[0rem]">
+          <div className="relative z-[1] flex flex-col items-center gap-[2.4rem] text-center">
+            <div data-aos="fade-up" className="relative">
+              <div className="absolute top-[-2rem] right-[1rem] w-[7%] md:top-[-3rem] md:right-[7rem]">
+                <img
+                  src={rays03}
+                  alt="icon"
+                  className="object-contain object-center"
+                />
+              </div>
+
+              <div className="absolute top-[8.5rem] left-[10rem] w-[35%] md:top-[5rem] md:left-[40rem] md:w-auto">
+                <img
+                  src={blueHighlight01}
+                  alt="Image"
+                  className="object-contain object-center"
+                />
+              </div>
+
+              <div className="max-w-[30rem] md:max-w-[70rem]">
+                <SectionTitle label="Trusted by Clients, Powered by Expertise" />
+              </div>
+            </div>
+
+            <div data-aos="fade-up">
+              <SectionDescription label="At nettax, we blend deep industry expertise with a client-first approach to deliver reliable, personalized financial and tax solutions." />
+            </div>
+
+            <div data-aos="fade-up">
+              <Button asChild>
+                <a href="https://wa.me/923192421501" target="_blank">
+                  <span>Get Free Consultation</span>
+
+                  <ChevronRight className="relative top-[3px] left-[.3rem] size-[1.5rem] stroke-3 transition-all duration-200 group-hover:left-[.6rem]" />
+                </a>
+              </Button>
+            </div>
           </div>
         </div>
 
